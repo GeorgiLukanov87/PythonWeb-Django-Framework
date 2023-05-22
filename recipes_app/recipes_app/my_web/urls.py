77f8662e038d8e3ce60ffbc7ff80b12deeb48f36
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-22 20:19:59 +0200

    config
    moving the templates in dir templates.
    create correct urls paths.

--- recipes_app/recipes_app/my_web/urls.py
+++ recipes_app/recipes_app/my_web/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = ()
+from django.urls import path
+
+from recipes_app.my_web.views import index, create, edit, delete, details
+
+urlpatterns = (
+    path('', index, name='index'),
+    path('create/', create, name='create'),
+
+    path('edit/<int:id>/', edit, name='edit'),
+    path('delete/<int:id>/', delete, name='delete'),
+    path('details/<int:id>/', details, name='details'),
+)
--- recipes_app/recipes_app/my_web/views.py
+++ recipes_app/recipes_app/my_web/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def create(request):
+    return render(request, 'create.html')
+
+
+def edit(request, pk):
+    return render(request, 'edit.html')
+
+
+def delete(request, pk):
+    return render(request, 'delete.html')
+
+
+def details(request, pk):
+    return render(request, 'details.html')
