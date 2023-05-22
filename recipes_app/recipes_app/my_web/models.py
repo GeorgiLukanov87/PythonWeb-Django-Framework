9a8db77a058a91fcf2ed910704d66b95ddddf336
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-22 20:50:21 +0200

    create new model recipe and migrate

--- recipes_app/recipes_app/my_web/models.py
+++ recipes_app/recipes_app/my_web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    RECIPE_MAX_LEN = 30
+    INGREDIENTS_MAX_LEN = 250
+    title = models.CharField(
+        max_length=RECIPE_MAX_LEN,
+    )
+    image_url = models.URLField()
+    description = models.TextField()
+    ingredients = models.CharField(
+        max_length=INGREDIENTS_MAX_LEN,
+    )
+    time = models.PositiveIntegerField()
