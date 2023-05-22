1e6966344d92fd2b9eca621a1fd8dd3dbae36b76
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-22 20:42:44 +0200

    templates
    base html + inheritance

--- recipes_app/recipes_app/my_web/urls.py
+++ recipes_app/recipes_app/my_web/urls.py
@@ -6,7 +6,7 @@ urlpatterns = (
     path('', index, name='index'),
     path('create/', create, name='create'),
 
-    path('edit/<int:id>/', edit, name='edit'),
-    path('delete/<int:id>/', delete, name='delete'),
-    path('details/<int:id>/', details, name='details'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('delete/<int:pk>/', delete, name='delete'),
+    path('details/<int:pk>/', details, name='details'),
 )
