9d750fb234510f8dc6a9f444540c8233f3843b57
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-23 09:44:26 +0200

    views urls

--- online_library_app/online_library_app/my_web/views.py
+++ online_library_app/online_library_app/my_web/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'common/home-no-profile.html')
+
+
+# book's views
+def add_book(request):
+    return render(request, 'book/add-book.html')
+
+
+def edit_book(request, pk):
+    return render(request, 'book/edit-book.html')
+
+
+def details_book(request, pk):
+    return render(request, 'book/book-details.html')
+
+
+# profile's views
+def profile_page(request):
+    return render(request, 'profile/profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile/edit-profile.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile/delete-profile.html')
