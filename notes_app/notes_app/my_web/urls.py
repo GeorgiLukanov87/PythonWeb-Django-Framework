53cefba2d95aa9e81619947cfa331301b68fe5f0
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-23 21:51:06 +0200

    urls and views
    create ursl and views , rendering templates without logic

--- notes_app/notes_app/my_web/urls.py
+++ notes_app/notes_app/my_web/urls.py
@@ -1,3 +1,26 @@
-urlpatterns = (
+from django.urls import path, include
 
-)
+from notes_app.my_web.views import index, add_note, edit_note, delete_note, details_note, profile_details
+
+# http://localhost:8000/ - home page
+
+# http://localhost:8000/add - add note page
+
+# http://localhost:8000/edit/1 - edit note page
+# http://localhost:8000/delete/1 - delete note page
+# http://localhost:8000/details/1 - note details page
+
+# http://localhost:8000/profile - profile page
+
+urlpatterns = [
+    path('', include([
+        path('', index, name='index'),
+        path('add/', add_note, name='add-note'),
+        path('edit/<int:pk>/', edit_note, name='edit-note'),
+        path('delete/<int:pk>/', delete_note, name='delete-note'),
+        path('details/<int:pk>/', details_note, name='details-note'),
+    ])),
+
+    path('profile/', profile_details, name='profile-details'),
+
+]
--- notes_app/notes_app/my_web/views.py
+++ notes_app/notes_app/my_web/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    profile = None
+    if profile:
+        return render(request, 'common/home-with-profile.html')
+    else:
+        return render(request, 'common/home-no-profile.html')
+
+
+def profile_details(request):
+    return render(request, 'common/profile.html')
+
+
+def add_note(request):
+    return render(request, 'note/note-create.html')
+
+
+def edit_note(request, pk):
+    return render(request, 'note/note-edit.html')
+
+
+def delete_note(request, pk):
+    return render(request, 'note/note-delete.html')
+
+
+def details_note(request, pk):
+    return render(request, 'note/note-details.html')
