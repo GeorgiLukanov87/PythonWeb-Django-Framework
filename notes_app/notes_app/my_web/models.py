adb9cf74a2eb9f29539890a019ee64c675601158
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-24 17:31:30 +0200

    create 2 models
    Profile model and Note model

--- notes_app/notes_app/my_web/models.py
+++ notes_app/notes_app/my_web/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    PROFILE_FIST_NAME_MAX_LEN = 20
+    PROFILE_LAST_NAME_MAX_LEN = 20
+
+    first_name = models.CharField(
+        max_length=PROFILE_FIST_NAME_MAX_LEN,
+    )
+
+    last_name = models.CharField(
+        max_length=PROFILE_LAST_NAME_MAX_LEN,
+    )
+
+    age = models.PositiveIntegerField()
+    image_url = models.URLField()
+
+
+class Note(models.Model):
+    NOTE_TITLE_MAX_LEN = 30
+    title = models.CharField(
+        max_length=NOTE_TITLE_MAX_LEN,
+    )
+
+    image_url = models.URLField()
+    content = models.TextField()
