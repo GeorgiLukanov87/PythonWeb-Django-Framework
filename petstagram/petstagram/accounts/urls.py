8177d00a011de0a97b353be1bc04f7de54f02d19
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-30 14:44:43 +0200

    add filter for placeholders

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path, include
 
-from petstagram.accounts.views import register, login, show_profile_details, edit_profile, delete_profile, out
+from petstagram.accounts.views import show_profile_details, edit_profile, delete_profile, to_github, SignInView, \
+    register
 
 # accounts/urls.py
 
 urlpatterns = (
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
-    path('out/', out, name='out'),
+    path('login/', SignInView.as_view(), name='login'),
+    path('to_github/', to_github, name='out'),
 
-path('profile/<int:pk>/', include(
-    [
-        path('', show_profile_details, name='profile details'),
-        path('edit/', edit_profile, name='profile edit'),
-        path('delete/', delete_profile, name='profile delete'),
-    ]
-)),
+    path('profile/<int:pk>/', include(
+        [
+            path('', show_profile_details, name='profile details'),
+            path('edit/', edit_profile, name='profile edit'),
+            path('delete/', delete_profile, name='profile delete'),
+        ]
+    )),
 
 )
--- petstagram/petstagram/accounts/views.py
+++ petstagram/petstagram/accounts/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+
+
+class SignInView(auth_views.LoginView):
+    template_name = 'accounts/login-page.html'
 
 
 def register(request):
@@ -22,5 +27,5 @@ def delete_profile(request, pk):
     return render(request, 'accounts/profile-delete-page.html')
 
 
-def out(request):
+def to_github(request):
     return HttpResponseRedirect("https://github.com/GeorgiLukanov87/PythonWeb-Django-Framework/tree/main/petstagram")
