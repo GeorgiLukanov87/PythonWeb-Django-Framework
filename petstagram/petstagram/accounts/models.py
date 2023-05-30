56f4d8afb5c2da037e5c71a34eea2be10ff9ca04
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-30 13:51:23 +0200

    create new User , extending the normal User with AbstractUser build in

--- petstagram/petstagram/accounts/models.py
+++ petstagram/petstagram/accounts/models.py
@@ -1,3 +1,58 @@
+from enum import Enum
+
+from django.contrib.auth import models as auth_models
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class ChoicesEnumMixin:
+    @classmethod
+    def choices(cls):
+        return [(x.name, x.value) for x in cls]
+
+    @classmethod
+    def max_len(cls):
+        return max(len(name) for name, _ in cls.choices())
+
+
+class Gender(ChoicesEnumMixin, Enum):
+    male = 'Male'
+    female = 'Female'
+    DoNotShow = 'Do not show'
+
+
+print(Gender.choices())
+print(Gender.max_len())
+
+
+class AppUser(auth_models.AbstractUser):
+    FIRST_NAME_MAX_LEN = 30
+    FIRST_NAME_MIN_LEN = 2
+
+    LAST_NAME_MAX_LEN = 30
+    LAST_NAME_MIN_LEN = 2
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LEN,
+        validators=(
+            validators.MinLengthValidator(FIRST_NAME_MIN_LEN),
+
+        )
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LEN,
+        validators=(
+            validators.MinLengthValidator(LAST_NAME_MIN_LEN),
+        )
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    gender = models.CharField(
+        choices=Gender.choices(),
+        max_length=Gender.max_len(),
+
+    )
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -106,3 +106,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+AUTH_USER_MODEL = 'accounts.AppUser'
