b1150814716e30bd63e5eb740ed25562f63c2e90
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-30 17:32:59 +0200

    fix admin user panel

--- petstagram/petstagram/accounts/admin.py
+++ petstagram/petstagram/accounts/admin.py
@@ -2,16 +2,62 @@ from django.contrib import admin
 
 from django.contrib.auth import admin as auth_admin, get_user_model
 
+from petstagram.accounts.forms import UserCreateForm, UserEditForm
+
 UserModel = get_user_model()
 
 
 @admin.register(UserModel)
 class UserAdmin(auth_admin.UserAdmin):
-    pass
+    form = UserEditForm
+    add_form = UserCreateForm
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'username',
+                    'password',
+                ),
+            }),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'gender'
+                ),
+            },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (
+            'Important dates',
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                ),
+            },
+        ),
+    )
 
 
 """"
-user1-
 
 gosho1
 gosho@asdf1234
