87def5409f6e7e3b21f633135b46a2a75e95c16f
Georgi St. Lukanov <goshiii87@gmail.com>
2023-05-30 13:57:10 +0200

    create superuser and register in admin

--- petstagram/petstagram/accounts/admin.py
+++ petstagram/petstagram/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth import admin as auth_admin, get_user_model
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class UserAdmin(auth_admin.UserAdmin):
+    pass
+
+
+""""
+gosho
+gosho@asdf1234
+"""
