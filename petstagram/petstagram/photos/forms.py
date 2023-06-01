c0aff5ada785e9b921af801722162938e9faf7eb
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-01 13:26:44 +0200

    logic for delete photo

--- petstagram/petstagram/photos/forms.py
+++ petstagram/petstagram/photos/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from petstagram.common.models import Like, Comment
+from petstagram.core.form_mixins import DisabledFormMixin
 from petstagram.photos.models import Photo
 
 
@@ -17,3 +19,22 @@ class PhotoEditForm(forms.ModelForm):
     class Meta:
         model = Photo
         exclude = ['photo']
+
+
+class PhotoDeleteForm(DisabledFormMixin, PhotoBaseForm):
+    disabled_fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.tagged_pets.clear()  # many-to-many
+
+            Photo.objects.all().first().tagged_pets.clear()
+            Like.objects.filter(to_photo_id=self.instance.id).delete()  # one-to-many
+            Comment.objects.filter(to_photo_id=self.instance.id).delete()  # one-to-many
+
+            self.instance.delete()
+
+        return self.instance
--- petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/photos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from petstagram.common.forms import CommentForm
-from petstagram.photos.forms import PhotoCreateForm, PhotoEditForm
+from petstagram.photos.forms import PhotoCreateForm, PhotoEditForm, PhotoDeleteForm
 from petstagram.photos.models import Photo
 
 
@@ -38,6 +39,7 @@ def show_photo_details(request, pk):
         'comments': comments,
         'comment_form': comment_form,
         'is_owner': request.user == photo.user,
+        # 'is_authenticated': request.user.is_active,
     }
 
     return render(
@@ -47,30 +49,68 @@ def show_photo_details(request, pk):
     )
 
 
-def edit_photo(request, pk):
-    photo = Photo.objects.get(pk=pk)
+# def edit_photo(request, pk):
+#     photo = Photo.objects.get(pk=pk)
+#
+#     if request.method == 'GET':
+#         form = PhotoEditForm(instance=photo)
+#     else:
+#         form = PhotoEditForm(request.POST, instance=photo)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('show photo details', pk=pk)
+#
+#     context = {
+#         'form': form,
+#         'photo': photo,
+#     }
+#
+#     return render(
+#         request,
+#         'photos/photo-edit-page.html',
+#         context,
+#     )
+#
+#
+# def delete_photo(request, pk):
+#     photo = Photo.objects.get(pk=pk)
+#     photo.delete()
+#     return redirect('show index')
 
-    if request.method == 'GET':
-        form = PhotoEditForm(instance=photo)
-    else:
-        form = PhotoEditForm(request.POST, instance=photo)
+
+def get_post_photo_form(request, form, success_url, template_path, pk=None):
+    if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect('show photo details', pk=pk)
+            return redirect(success_url)
 
     context = {
         'form': form,
-        'photo': photo,
+        'pk': pk,
     }
 
-    return render(
+    return render(request, template_path, context)
+
+
+def edit_photo(request, pk):
+    photo = Photo.objects.filter(pk=pk) \
+        .get()
+    return get_post_photo_form(
         request,
-        'photos/photo-edit-page.html',
-        context,
+        PhotoEditForm(request.POST or None, instance=photo),
+        success_url=reverse('show index'),
+        template_path='photos/photo-edit-page.html',
+        pk=pk,
     )
 
 
 def delete_photo(request, pk):
-    photo = Photo.objects.get(pk=pk)
-    photo.delete()
-    return redirect('show index')
+    photo = Photo.objects.filter(pk=pk) \
+        .get()
+    return get_post_photo_form(
+        request,
+        PhotoDeleteForm(request.POST or None, instance=photo),
+        success_url=reverse('show index'),
+        template_path='photos/photo-delete-page.html',
+        pk=pk,
+    )
