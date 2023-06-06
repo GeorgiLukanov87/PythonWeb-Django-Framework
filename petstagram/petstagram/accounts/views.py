08b31290cba2eb995c13ccbb03380145f5c37168
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-06 18:57:10 +0200

    paginator added

--- petstagram/petstagram/accounts/views.py
+++ petstagram/petstagram/accounts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
 from django.contrib.auth import views as auth_views, get_user_model, login
 from django.urls import reverse_lazy
@@ -31,6 +32,16 @@ class SignOutView(auth_views.LogoutView):
 class UserDetailsView(generic.DetailView):
     model = UserModel
     template_name = 'accounts/profile-details-page.html'
+    photos_paginate_by = 2
+
+    def get_photos_page(self):
+        return self.request.GET.get('page', 1)
+
+    def get_paginated_photos(self):
+        page = self.get_photos_page()
+        photos = self.object.photo_set.all()
+        paginator = Paginator(photos, self.photos_paginate_by)
+        return paginator.get_page(page)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -45,6 +56,9 @@ class UserDetailsView(generic.DetailView):
         context['likes_count'] = sum(x.like_set.count() for x in photos)
 
         context['pets'] = self.object.pet_set.all()
+
+        # Paginator
+        context['photos'] = self.get_paginated_photos()
 
         return context
 
