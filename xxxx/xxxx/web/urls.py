f7e45704467f7ad8e14258d6e092fdfed993c30d
Georgi St. Lukanov <102332504+GeorgiLukanov87@users.noreply.github.com>
2023-06-19 00:55:29 +0200

    Delete xxxx directory

--- xxxx/xxxx/web/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from xxxx.web.views import index
-
-urlpatterns = (
-    path('', index, name='index'),
-)
--- xxxx/xxxx/web/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def index(request):
-    html = 'work'
-    return HttpResponse(html)
